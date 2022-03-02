e26683f9a64193d459b090f49b6333484920cf75
Antosha Baboshin <antosha.js@gmail.com>
2022-03-02 13:42:09 +0300

    add morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let mainArray = [];
+  let lettersArray = [];
+  let resMorseArray = [];
+  let resLetters = [];
+  let finalArray = '';
+  
+  for (i = 0; i < expr.length; i += 10) {
+    mainArray.push(expr.slice(i, i + 10));
+  }
+  resMorseArray = mainArray.map(el => morse(el));
+  
+  for (key in resMorseArray) 
+    if (MORSE_TABLE.hasOwnProperty(resMorseArray[key])) {
+      finalArray = finalArray + MORSE_TABLE[resMorseArray[key]];
+    } else {
+      finalArray = finalArray + ' ';
+    }
+    function morse(string) {
+  let morseArray = [];
+  let letter = '';
+  
+  for (i = 0; i < 10; i += 2) {
+    morseArray.push(string.slice(i, i + 2))
+  }
+  for (j = 0; j < 5; j += 1) {
+    if (morseArray[j] == '00') {
+      morseArray[j] = '';
+    } else if (morseArray[j] == '10') {
+      morseArray[j] = '.';
+    } else if (morseArray[j] == '11') {
+      morseArray[j] = '-';
+    }
+  }
+  letter = morseArray.join('');
+  return letter;
 }
+  return finalArray;
+  }
 
 module.exports = {
     decode
